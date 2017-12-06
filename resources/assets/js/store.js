40ac95cd7f3f27ec63e0abc99d0c286a9a8fa73c
Mark Biek <support@janustech.net>
2017-12-06 11:21:27 -0500

    Tweak to work with Redux dev tools

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -9,6 +9,6 @@ const reducers = combineReducers({
     metaState: companyReducer
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
