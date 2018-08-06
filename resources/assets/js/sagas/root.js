4d8ebbce966ed91ea89a9ed7fe9447621cfec52e
Mark Biek <support@janustech.net>
2018-08-06 09:51:11 -0400

    Initial saga setup

---
+++ resources/assets/js/sagas/root.js
@@ -0,0 +1,9 @@
+import { put, takeEvery, call, all, select } from 'redux-saga/effects'
+
+import store from '../store';
+
+export default function* rootSaga() {
+    yield all([
+        //
+    ]);
+}
--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import rootSaga from './sagas/root';
 
 import { metaReducer } from './modules/Meta/reducer';
 import { invoiceReducer } from './modules/Invoices/reducer';
 import { taskReducer } from './modules/Tasks/reducer';
 import { companyReducer } from './modules/Companies/reducer';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const reducers = combineReducers({
     metaState: metaReducer,
@@ -16,7 +20,9 @@ const reducers = combineReducers({
 const store = createStore(
     reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
+    applyMiddleware(sagaMiddleware)
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
